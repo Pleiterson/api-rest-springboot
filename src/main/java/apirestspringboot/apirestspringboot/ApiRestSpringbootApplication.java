be335fade4dff9095a0a8c55644f0f8e8e027e79
pleiterson <pleiterson@gmail.com>
2021-01-14 16:47:08 -0300

    first commit

---
+++ src/main/java/apirestspringboot/apirestspringboot/ApiRestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package apirestspringboot.apirestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apirestspringboot/apirestspringboot/ApiRestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package apirestspringboot.apirestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
